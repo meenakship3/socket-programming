6c64c4ec7843e93def25b2218dad517704503ac8
meenakship3 <meenakshi.pradeep1@gmail.com>
2025-06-06 17:21:04 +0530

    add webserver.py and modify webclient.py

--- client-and-server/webclient.py
+++ client-and-server/webclient.py
@@ -1,8 +1,15 @@
 import socket
 import sys
 
+"""
+Client sends ISO-8858-1 encoded request to a webserver and prints decoded response. Host and port can be entered by user.
+"""
 host = sys.argv[1]
-port = 80 or sys.argv[2]
+if len(sys.argv) > 2:
+    port = int(sys.argv[2])
+else:
+    port = 80
+
 client_socket = socket.socket()
 try:
     client_socket.connect((host, port))
@@ -10,7 +17,7 @@ except socket.error as e:
     print(f"Error connecting: {e}")
     exit()
 
-http_request = f"GET / HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\n"
+http_request = f"GET / HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n"
 
 client_socket.sendall(http_request.encode("ISO-8859-1"))
 
---
+++ client-and-server/webserver.py
@@ -0,0 +1,44 @@
+import socket
+import sys
+
+"""
+Server that runs forever, listening for incoming requests. Sends back a simple response.
+"""
+
+if len(sys.argv) > 1:
+    port = int(sys.argv[1])
+else:
+    port = 28333
+
+print(f"Starting server on port {port}")
+
+server_socket = socket.socket()
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind(('',port))
+server_socket.listen()
+
+try:
+    while True:
+        new_conn = server_socket.accept()
+        new_socket = new_conn[0]
+
+        request = b''
+        while True:
+            data = new_socket.recv(1024)
+            if not data:
+                break
+            request += data
+            if b'\r\n\r\n' in request:
+                break
+        print(request.decode("ISO-8859-1"))
+
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 5\r\nConnection: close\r\n\r\nHello"
+
+        new_socket.send(response.encode("ISO-8859-1"))
+        new_socket.close()
+
+finally:
+    server_socket.close()
+
+
